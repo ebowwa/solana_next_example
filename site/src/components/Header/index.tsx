8c231a2a639a6c18c35b354de937d716a959b25d
ebowwa <81942069+ebowwa@users.noreply.github.com>
2024-07-09 23:03:31 +0000

    fix

--- site/src/components/Header/index.tsx
+++ site/src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import GlitchText from '@/components/GlitchText';
 export const Header = () => (
   <div className="py-4 px-2 sm:py-6 sm:px-4 md:py-8 md:px-6 text-center overflow-x-hidden">
     <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold mb-4 sm:mb-6 md:mb-8 bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-600 break-words">
-      <GlitchText text="Solana Wallet Dashboard" className="header-glitch" />
+      <GlitchText text="Solana Wallet" className="header-glitch" />
     </h1>
   </div>
 );
