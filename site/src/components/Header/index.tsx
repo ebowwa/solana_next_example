88fca54a86a094fc0e9da9ba933146b8ddf04e92
ebowwa <81942069+ebowwa@users.noreply.github.com>
2024-07-09 22:57:57 +0000

    mobile friendly push

--- site/src/components/Header/index.tsx
+++ site/src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import GlitchText from '@/components/GlitchText';
 
 export const Header = () => (
-    <h1 className="text-6xl font-extrabold mb-8 text-center bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-600">
-      <GlitchText text="Solana Wallet Dashboard" className="header-glitch" />
-    </h1>
+  <h1 className="text-4xl sm:text-5xl md:text-6xl font-extrabold mb-8 text-center bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-600">
+    <GlitchText text="Solana Wallet Dashboard" className="header-glitch" />
+  </h1>
 );
